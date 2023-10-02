5fe05abafddd2c83572607b57a8cb437dcf12ae6
Huynh Nhat Cuong <cuonghuynhnhat2025@gmail.com>
2023-10-03 01:33:00 +0700

    LoadMap

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import vnTopo from './geo_data/mapvn1.json' ;
+import hs from './geo_data/Hoangsa.json' ;
+import ts from './geo_data/Truongsa.json' ;
+import {
+  ComposableMap,
+  Geographies,
+  Geography,
+  ZoomableGroup,
+} from 'react-simple-maps';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hallo
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const vietnam = [vnTopo, hs, ts];
+
+
+class App extends Component {
+  render() {
+    return (
+      <ComposableMap
+      projection="geoMercator"
+      projectionConfig={{
+        scale: 1000,
+        center: [105, 15] // coordinate of VietNam [long, lat]
+      }}
+      style={{
+        width: "100%",
+        height: "auto"
+      }}
+    >
+      <ZoomableGroup center={[104, 17]}>
+        {vietnam.map((geoUrl) => (
+          <Geographies key={geoUrl} geography={geoUrl}>
+            {({ geographies }) =>
+              geographies.map((geo) => (
+                <Geography
+                  key={geo.rsmKey}
+                  geography={geo}
+                  style={{
+                    default: {
+                      fill: '#808080',
+                      stroke: '#212529',
+                      strokeWidth: 0.75,
+                      outline: 'none',
+                    },
+                    hover: {
+                      fill: '#e6dfd9',
+                      stroke: '#212529',
+                      strokeWidth: 0.75,
+                      outline: 'none',
+                    },
+                  }}
+                />
+              ))
+            }
+          </Geographies>
+        ))}
+      </ZoomableGroup>
+    </ComposableMap>
+    );
+  }
 }
 
 export default App;
+
