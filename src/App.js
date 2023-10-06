24268db5d5330d1b594d2c956462f24a78afb4e5
TrongHieu0405 <hieunguyentrong045@gmail.com>
2023-10-06 21:06:44 +0700

    react

--- src/App.js
+++ src/App.js
@@ -221,7 +221,8 @@ class App extends Component {
              </div>
         </div>
     </section>
-    <section className="destination">
+
+ <section className="destination">
         <div className="container">
             <h2 className="destination-title">Top Destinations</h2>
             <div className="destination-wrapper">
@@ -364,6 +365,151 @@ class App extends Component {
             </div>
         </div>
     </section>
+
+    <section className="destination">
+        <div className="container">
+            <h2 className="destination-title">Top Blogs</h2>
+            <div className="destination-wrapper">
+                <a href="#" className="slider-arrow prev" data-slide="prev" data-target="#destination-list"><i
+                        className="ri-arrow-left-s-line"></i></a>
+                <a href="#" className="slider-arrow next" data-slide="next" data-target="#destination-list"><i
+                        className="ri-arrow-right-s-line"></i></a>
+                <div className="destination-list" id="destination-list">
+                    <div>
+                        <div className="destination-list-top">
+                            <img src="images/hero-bg-1.webp" />
+                            <a className="destinations-list-top-favourite">
+                                <i className="ri-heart-3-line"></i>
+                            </a>
+                            <span className="destinations-list-top-tag">Popular</span>
+                        </div>
+                        <div className="destination-list-content">
+                            <div className="destination-list-content-location">
+                                <i className="ri-global-line"></i>
+                                Marrakech, Morocco
+                            </div>
+                            <a className="destination-list-content-title" href="#">Explore the Magic of Marrakech</a>
+                            <div className="destination-list-content-rating">
+                                <i className="ri-star-fill"></i>
+                                <i className="ri-star-fill"></i>
+                                <i className="ri-star-fill"></i>
+                                <i className="ri-star-half-fill"></i>
+                            </div>
+                            <div className="destination-list-content-price">
+                                from <span>$123</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <div className="destination-list-top">
+                            <img src="images/hero-bg-1.webp" />
+                        </div>
+                        <div className="destination-list-content">
+                            <div className="destination-list-content-location">
+                                <i className="ri-global-line"></i>
+                                Marrakech, Morocco
+                            </div>
+                            <a className="destination-list-content-title" href="#">Explore the Magic of Marrakech</a>
+                            <div className="destination-list-content-rating">
+                                <i className="ri-star-fill"></i>
+                                <i className="ri-star-fill"></i>
+                                <i className="ri-star-fill"></i>
+                                <i className="ri-star-half-fill"></i>
+                            </div>
+                            <div className="destination-list-content-price">
+                                from <span>$123</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <div className="destination-list-top">
+                            <img src="images/hero-bg-1.webp" />
+                        </div>
+                        <div className="destination-list-content">
+                            <div className="destination-list-content-location">
+                                <i className="ri-global-line"></i>
+                                Marrakech, Morocco
+                            </div>
+                            <a className="destination-list-content-title" href="#">Explore the Magic of Marrakech</a>
+                            <div className="destination-list-content-rating">
+                                <i className="ri-star-fill"></i>
+                                <i className="ri-star-fill"></i>
+                                <i className="ri-star-fill"></i>
+                                <i className="ri-star-half-fill"></i>
+                            </div>
+                            <div className="destination-list-content-price">
+                                from <span>$123</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <div className="destination-list-top">
+                            <img src="images/hero-bg-1.webp" />
+                        </div>
+                        <div className="destination-list-content">
+                            <div className="destination-list-content-location">
+                                <i className="ri-global-line"></i>
+                                Marrakech, Morocco
+                            </div>
+                            <a className="destination-list-content-title" href="#">Explore the Magic of Marrakech</a>
+                            <div className="destination-list-content-rating">
+                                <i className="ri-star-fill"></i>
+                                <i className="ri-star-fill"></i>
+                                <i className="ri-star-fill"></i>
+                                <i className="ri-star-half-fill"></i>
+                            </div>
+                            <div className="destination-list-content-price">
+                                from <span>$123</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <div className="destination-list-top">
+                            <img src="images/hero-bg-1.webp" />
+                        </div>
+                        <div className="destination-list-content">
+                            <div className="destination-list-content-location">
+                                <i className="ri-global-line"></i>
+                                Marrakech, Morocco
+                            </div>
+                            <a className="destination-list-content-title" href="#">Explore the Magic of Marrakech</a>
+                            <div className="destination-list-content-rating">
+                                <i className="ri-star-fill"></i>
+                                <i className="ri-star-fill"></i>
+                                <i className="ri-star-fill"></i>
+                                <i className="ri-star-half-fill"></i>
+                            </div>
+                            <div className="destination-list-content-price">
+                                from <span>$123</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <div className="destination-list-top">
+                            <img src="images/hero-bg-1.webp" />
+                        </div>
+                        <div className="destination-list-content">
+                            <div className="destination-list-content-location">
+                                <i className="ri-global-line"></i>
+                                Marrakech, Morocco
+                            </div>
+                            <a className="destination-list-content-title" href="#">Explore the Magic of Marrakech</a>
+                            <div className="destination-list-content-rating">
+                                <i className="ri-star-fill"></i>
+                                <i className="ri-star-fill"></i>
+                                <i className="ri-star-fill"></i>
+                                <i className="ri-star-half-fill"></i>
+                            </div>
+                            <div className="destination-list-content-price">
+                                from <span>$123</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    
     <section className="testimonial">
         <div className="container">
             <h2 className="section-title">Join the Adventure, Read Our Reviews</h2>
